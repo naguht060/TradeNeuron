5475bc658c4ce9eecce586f0efdb6b8ffbc285aa
naguht060 <nagaraj.tulasigeri@gmail.com>
2026-05-30 01:17:54 +0530

    Added initail code

---
+++ utils/version.py
@@ -0,0 +1,13 @@
+# TradeNeuron Version Management
+# This file is the single source of truth for version information
+
+VERSION = "2.0.1.1"
+
+
+def get_version() -> str:
+    """Return the current TradeNeuron version.
+
+    Returns:
+        str: The current version string (e.g. '2.0.0.2')
+    """
+    return VERSION
